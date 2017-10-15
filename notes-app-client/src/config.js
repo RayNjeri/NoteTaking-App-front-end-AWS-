56e9801a0140561ff2cd1785c2797c80ead02f4f
Rachael Njeri <rachael.njeri@andela.com>
2017-10-15 18:14:18 +0300

    modify the login to use AWS cognito user pool
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

---
+++ notes-app-client/src/config.js
@@ -0,0 +1,6 @@
+export default {
+  cognito: {
+    USER_POOL_ID: "us-east-1_E4Ps2PuAJ",
+    APP_CLIENT_ID: "5mtr65usg1j8qmvq1ndm4c8cf7"
+  }
+}
--- notes-app-client/src/containers/Login.js
+++ notes-app-client/src/containers/Login.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import config from "../config"
+import { CognitoUserPool, AuthenticationDetails, CognitoUser} from "amazon-cognito-identity-js";
 import "./Login.css";
 
 export default class Login extends Component {
@@ -11,6 +13,24 @@ export default class Login extends Component {
       password: ""
     };
   }
+
+  login(email, password) {
+    const userPool = new CognitoUserPool({
+      UserPoolId: config.cognito.USER_POOL_ID,
+      ClientId: config.cognito.APP_CLIENT_ID
+    });
+    const user = new CognitoUser({ Username: email, Pool: userPool });
+    const authenticationData = { Username: email, Password: password };
+    const authenticationDetails = new AuthenticationDetails(authenticationData);
+
+    return new Promise((resolve, reject) =>
+      user.authenticateUser(authenticationDetails, {
+        onSuccess: result => resolve(),
+        onFailure: err => reject(err)
+      })
+    );
+  }
+
   validateForm() {
     return this.state.email.length > 0 && this.state.password.length > 0;
   }
@@ -19,9 +39,22 @@ export default class Login extends Component {
       [event.target.id]: event.target.value
     });
   }
-  handleSubmit = event => {
+
+  handleSubmit = async event => {
     event.preventDefault();
+
+    this.setState({ isLoading: true });
+
+    try {
+      await this.login(this.state.email, this.state.password);
+      this.props.userHasAuthenticated(true);
+      console.log(">>>>", this.props.userHasAuthenticated)
+    } catch (e) {
+      alert(e);
+      this.setState({ isLoading: false });
+    }
   }
+  
   render() {
     return (
       <div className="Login">
