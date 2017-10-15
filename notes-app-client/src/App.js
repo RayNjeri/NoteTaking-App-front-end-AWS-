7256afb2b36b3771cbb2ba3121661fbb59fd0566
Rachael Njeri <rachael.njeri@andela.com>
2017-10-15 16:09:46 +0300

    add links to navbar
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- notes-app-client/src/App.js
+++ notes-app-client/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 import Routes from "./Routes";
+import RouteNavItem from "./components/RouteNavItem";
 import "./App.css";
 
 class App extends Component {
@@ -17,8 +18,8 @@ class App extends Component {
           </Navbar.Header>
           <Navbar.Collapse>
             <Nav pullRight>
-              <NavItem href="/signup">Signup</NavItem>
-              <NavItem href="/login">Login</NavItem>
+              <RouteNavItem href="/signup">Signup</RouteNavItem>
+              <RouteNavItem href="/login">Login</RouteNavItem>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
