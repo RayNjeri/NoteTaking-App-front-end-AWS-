2f159072cef534310acf53ed25e84809a56b06be
Rachael Njeri <rachael.njeri@andela.com>
2017-10-17 18:14:27 +0300

    Add the Notes container

--- notes-app-client/src/containers/Home.js
+++ notes-app-client/src/containers/Home.js
@@ -9,12 +9,57 @@ export default class Home extends Component {
     super(props);
 
     this.state = {
-      isLoading:
-      true, notes: []
+      isLoading: true,
+      notes: []
     };
   }
+
+  async componentDidMount() {
+    if (!this.props.isAuthenticated) {
+      return;
+    }
+
+    try {
+      const results = await this.notes();
+      this.setState({ notes: results });
+    } catch (e) {
+      alert(e);
+    }
+
+    this.setState({ isLoading: false });
+  }
+
+  notes() {
+    return invokeApig({ path: "/notes" });
+  }
+
+  handleNoteClick = event => {
+    event.preventDefault();
+    this.props.history.push(event.currentTarget.getAttribute("href"));
+  }
+
   renderNotesList(notes) {
-    return null;
+    return [{}].concat(notes).map(
+      (note, i) =>
+        i !== 0
+          ? <ListGroupItem
+            key={note.noteId}
+            href={`/notes/${note.noteId}`}
+            onClick={this.handleNoteClick}
+            header={note.content.trim().split("\n")[0]}
+          >
+            {"Created: " + new Date(note.createdAt).toLocaleString()}
+          </ListGroupItem>
+          : <ListGroupItem
+            key="new"
+            href="/notes/new"
+            onClick={this.handleNoteClick}
+          >
+            <h4>
+              <b>{"\uFF0B"}</b> Create a new note
+              </h4>
+          </ListGroupItem>
+    );
   }
 
   renderLander() {
@@ -22,9 +67,18 @@ export default class Home extends Component {
       <div className="lander">
         <h1>Scratch</h1>
         <p>A simple note taking app</p>
+        <div>
+          <Link to="/login" className="btn btn-info btn-lg">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg">
+            Signup
+          </Link>
+        </div>
       </div>
     );
   }
+
   renderNotes() {
     return (
       <div className="notes">
@@ -35,6 +89,7 @@ export default class Home extends Component {
       </div>
     );
   }
+
   render() {
     return (
       <div className="Home">
@@ -42,5 +97,4 @@ export default class Home extends Component {
       </div>
     );
   }
-  
 }
--- notes-app-client/src/containers/NewNote.js
+++ notes-app-client/src/containers/NewNote.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
+import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
 import { invokeApig, s3Upload } from "../libs/awsLib";
 import config from "../config";
@@ -17,26 +17,16 @@ export default class NewNote extends Component {
     };
   }
 
-  async componentDidMount() {
-    if (!this.props.isAuthenticated) {
-      return;
-    }
-    try {
-      const results = await this.notes();
-      this.setState({ notes: results });
-    } catch (e) {
-      alert(e);
-    }
-    this.setState({ isLoading: false });
-  }
-
-  notes() {
-    return invokeApig({ path: "/notes" });
-
-  }
-
   validateForm() {
     return this.state.content.length > 0;
+  }
+
+  createNote(note) {
+    return invokeApig({
+      path: "/notes",
+      method: "POST",
+      body: note
+    });
   }
 
   handleChange = event => {
@@ -53,15 +43,17 @@ export default class NewNote extends Component {
     event.preventDefault();
 
     if (this.file && this.file.size > config.MAX_ATTACHMENT_SIZE) {
+      alert("Please pick a file smaller than 5MB");
       return;
     }
+
     this.setState({ isLoading: true });
 
     try {
       const uploadedFilename = this.file
         ? (await s3Upload(this.file)).Location
         : null;
-      
+
       await this.createNote({
         content: this.state.content,
         attachment: uploadedFilename
@@ -71,35 +63,35 @@ export default class NewNote extends Component {
       alert(e);
       this.setState({ isLoading: false });
     }
-
-    
   }
 
-  renderNotesList(notes) {
-    return [{}].concat(notes).map(
-      (note, i) =>
-        i !== 0
-          ? <ListGroupItem
-            key={note.noteId}
-            href={`/notes/${note.noteId}`}
-            onClick={this.handleNoteClick}
-            header={note.content.trim().split("\n")[0]}
-          >
-            {"Created: " + new Date(note.createdAt).toLocaleString()}
-          </ListGroupItem>
-          : <ListGroupItem
-            key="new"
-            href="/notes/new"
-            onClick={this.handleNoteClick}
-          >
-            <h4>
-              <b>{"\uFF0B"}</b> Create a new note
-              </h4>
-          </ListGroupItem>
+  render() {
+    return (
+      <div className="NewNote">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="content">
+            <FormControl
+              onChange={this.handleChange}
+              value={this.state.content}
+              componentClass="textarea"
+            />
+          </FormGroup>
+          <FormGroup controlId="file">
+            <ControlLabel>Attachment</ControlLabel>
+            <FormControl onChange={this.handleFileChange} type="file" />
+          </FormGroup>
+          <LoaderButton
+            block
+            bsStyle="primary"
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+            isLoading={this.state.isLoading}
+            text="Create"
+            loadingText="Creating…"
+          />
+        </form>
+      </div>
     );
-  }
-  handleNoteClick = event => {
-    event.preventDefault();
-    this.props.history.push(event.currentTarget.getAttribute("href"));
   }
 }
