6a3a8b91c4ddc8cbdd3fbc87e5dab3f694a71a90
Rachael Njeri <rachael.njeri@andela.com>
2017-10-17 14:21:21 +0300

    create sign up form
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- notes-app-client/src/containers/Login.js
+++ notes-app-client/src/containers/Login.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
-import {FormGroup, FormControl, ControlLabel } from "react-bootstrap";
-import config from "../config"
-import { CognitoUserPool, AuthenticationDetails, CognitoUser } from "amazon-cognito-identity-js";
+import {
+  CognitoUserPool,
+  AuthenticationDetails,
+  CognitoUser
+} from "amazon-cognito-identity-js";
+import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
+import config from "../config";
 import "./Login.css";
 
 export default class Login extends Component {
@@ -10,6 +14,7 @@ export default class Login extends Component {
     super(props);
 
     this.state = {
+      isLoading: false,
       email: "",
       password: ""
     };
@@ -35,6 +40,7 @@ export default class Login extends Component {
   validateForm() {
     return this.state.email.length > 0 && this.state.password.length > 0;
   }
+
   handleChange = event => {
     this.setState({
       [event.target.id]: event.target.value
@@ -49,44 +55,44 @@ export default class Login extends Component {
     try {
       await this.login(this.state.email, this.state.password);
       this.props.userHasAuthenticated(true);
-      this.props.history.push("/");
     } catch (e) {
       alert(e);
       this.setState({ isLoading: false });
     }
   }
-  
+
   render() {
     return (
       <div className="Login">
         <form onSubmit={this.handleSubmit}>
-        <FormGroup controlId="email" bsSize="large">
-          <ControlLabel>Email</ControlLabel>
-          <FormControl
-          autoFocus
-          type="email"
-          value={this.state.email}
-          onChange={this.handleChange}
-          />
-        </FormGroup>
-        <FormGroup controlId="password" bsSize="large">
-          <ControlLabel>Password</ControlLabel>
-          <FormControl
-          value={this.state.password}
-          onChange={this.handleChange}
-          type="password"
+          <FormGroup controlId="email" bsSize="large">
+            <ControlLabel>Email</ControlLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
             />
-        </FormGroup>
-        <LoaderButton
-        block
-        bsSize="large"
-        disabled={!this.validateForm()}
-        type="submit"
-        text="Login" 
-        loadingText="Logging in..."    
-        />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+            />
+          </FormGroup>
+          <LoaderButton
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+            isLoading={this.state.isLoading}
+            text="Login"
+            loadingText="Logging in…"
+          />
         </form>
-        </div>
+      </div>
     );
   }
 }
